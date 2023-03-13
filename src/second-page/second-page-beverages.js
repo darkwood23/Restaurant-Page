fdd99f6fbb660ae8c4f7edbded9684f2ffef3123
darkwood23 <anonymoushackerintheweb@gmail.com>
2023-03-13 20:47:16 +0545

    Beverages done

--- src/second-page/second-page-beverages.js
+++ src/second-page/second-page-beverages.js
@@ -2,10 +2,12 @@ function displayBeverages(contentHold, img1, img2) {
     const divBeverages = document.createElement("div")
     const title = document.createElement("h2")
     const bloodyMaria = document.createElement("div")
+    const bloodyMariaName = document.createElement("h4")
     const bloodyMariaDetails = document.createElement("p")
     const bloodyMariaPrice = document.createElement("h3")
     const bloodyMariaPic = new Image()
     const spicyPaloma = document.createElement("div")
+    const spicyPalomaName = document.createElement("h4")
     const spicyPalomaDetails = document.createElement("p")
     const spicyPalomaPic = new Image()
     const spicyPalomaPrice = document.createElement("h3")
@@ -18,6 +20,8 @@ function displayBeverages(contentHold, img1, img2) {
     spicyPaloma.id = "spicy-paloma"
 
     title.textContent = "Beverages"
+    spicyPalomaName.textContent = "Spicy Paloma"
+    bloodyMariaName.textContent = "Bloody Maria"
 
     bloodyMariaDetails.textContent = "When you switch from vodka to tequila, you transform the famous bloody mary into the equally tasty bloody maria. It's the same great drink, but the tequila background makes a big difference."
     bloodyMariaPic.src = img1
@@ -27,9 +31,11 @@ function displayBeverages(contentHold, img1, img2) {
     spicyPalomaPic.src = img2
     spicyPalomaPrice.textContent = "Rs.250"
 
+    bloodyMaria.appendChild(bloodyMariaName)
     bloodyMaria.appendChild(bloodyMariaDetails)
     bloodyMaria.appendChild(bloodyMariaPic)
     bloodyMaria.appendChild(bloodyMariaPrice)
+    spicyPaloma.appendChild(spicyPalomaName)
     spicyPaloma.appendChild(spicyPalomaDetails)
     spicyPaloma.appendChild(spicyPalomaPic)
     spicyPaloma.appendChild(spicyPalomaPrice)
